d482c016865515fc3daea8f97f3a535467cc9caf
João Marcos Cardoso de Oliveira <117744361+Joao-MCO@users.noreply.github.com>
2022-11-27 12:24:40 -0300

    Terceiro Commit
    Remoção de readFile e writeFile e inclusão de todo processo com FILE na main

--- main.c
+++ main.c
@@ -9,59 +9,39 @@ const char palavrasReservadas[14][50] = {"qqhavelinho", "issoetudopessoal","char
                                             "loolooloop", "int", "real", "toonIn", "toonOut"};
 
 
-int readFile(FILE *arq, char *destino[240], char caminho[240], char tipo[3]);
-void writeFile(char **texto, FILE *arq, int tamanho, char caminho[240], char tipo[3]);
-
 int main(int argc, char const *argv[]){
-    FILE *arq;
-    int tamanho;
+    //Declaração das variáveis 
+    int cont = 0;
+    FILE *arq1, *arq2;
+    char texto[1000][240];
     char caminho[240];
-    char** destino;
-    destino =  (char **) malloc(1000 * sizeof(char *));
-    int i;
-    for(i=0;i<1000;i++){
-        destino[i] = (char *) malloc(240*sizeof(char));
-    }
     gets(caminho);
-    tamanho = readFile(arq, destino, caminho, "r");
-    // openFile("saida.txt", arq, "w+");
-    writeFile(destino, arq, tamanho, "output.txt", "w+");
-    return 0;
-}
-
-int readFile(FILE *arq, char *destino[240], char caminho[240], char tipo[3]){
-    arq = fopen(caminho, tipo);
-    if(arq == NULL){
+    //Após receber qual o caminho do arquivo de entrada, é hora de ler linha a linha e ar
+    arq1 = fopen(caminho, "r");
+    if(arq1 == NULL){
         printf("\nO arquivo %s nao aberto!! Tente novamente...\n", caminho);
     }else{
-        printf("\nO arquivo %s foi aberto com sucesso com o metodo %s!\n", caminho, tipo);
+        printf("\nO arquivo %s foi aberto com sucesso!\n", caminho);
     }
-    int cont = 0;
-    char texto[1000][240];
-    printf("Parte 1\n");
-    while(!feof(arq)){
-        fgets(texto[cont], 240, arq);
+    while(!feof(arq1)){
+        fgets(texto[cont], 240, arq1);
         printf("%s", texto[cont]);
         cont++;
     }
-    fclose(arq);
     printf("\nDados lidos com sucesso!\n");
-    **destino = **texto;
-    return cont;
-}
-
-void writeFile(char **texto, FILE *arq, int tamanho, char caminho[240], char tipo[3]){
-    arq = fopen(caminho, tipo);
-    if(arq == NULL){
+    arq2 = fopen("output.txt", "w+");
+    if(arq2 == NULL){
         printf("\nO arquivo %s nao aberto!! Tente novamente...\n", caminho);
     }else{
-        printf("\nO arquivo %s foi aberto com sucesso com o metodo %s!\n", caminho, tipo);
+        printf("\nO arquivo %s foi aberto com sucesso!\n", caminho);
     }
     int i;
-    for(i=0;i<tamanho;i++){
-        printf("%s\n", texto[i], i, tamanho);
-        fprintf(arq, "%s", texto[i]);
+    for(i=0;i<cont;i++){
+        printf("%s\n", texto[i]);
+        fprintf(arq2, "%s", texto[i]);
     }
-    fclose(arq);
+    fclose(arq1);
+    fclose(arq2);
     printf("Dados gravados com sucesso!\n");
+    return 0;
 }
